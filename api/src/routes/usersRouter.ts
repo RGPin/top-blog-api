4d0ebb5feb7c12c00bba520513fd8820b5fcaf0d
RGPin <157803400+RGPin@users.noreply.github.com>
2026-07-10 17:29:59 +0800

    auth plan, protectRoute middleware

--- api/src/routes/usersRouter.ts
+++ api/src/routes/usersRouter.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import * as controller from "../controllers/usersController.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => res.send("Users"));
+router.post("/signup", controller.postSignup);
+router.post("/login", controller.postLogin);
+router.delete("/logout", controller.deleteLogout);
 
 export default router;
