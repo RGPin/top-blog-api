f3cb31b3f57c42b83e3d2a719fa44cf5883662b8
RGPin <157803400+RGPin@users.noreply.github.com>
2026-07-11 19:47:58 +0800

    prevent prisma from creating new connection with every hot reload

--- api/src/db/prismaInstance.ts
+++ api/src/db/prismaInstance.ts
@@ -1,8 +1,23 @@
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
+// prevent prisma from creating new connection with every hot reload
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export const prisma = new PrismaClient({ adapter });
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter: new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    }),
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
