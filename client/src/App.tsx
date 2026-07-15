21bf1be752e921c95fa74c74f5509db41d7a8c71
RGPin <157803400+RGPin@users.noreply.github.com>
2026-07-15 11:14:13 +0800

    plan pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,22 @@
 import { Route, Routes } from "react-router";
 import "./App.css";
+import Header from "./components/Header/Header";
+import MainPage from "./pages/MainPage/MainPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import SignupPage from "./pages/SignupPage/SignupPage";
+import PostDetails from "./pages/PostDetails/PostDetails";
 
 function App() {
   return (
-    <>
+    <div className="App">
+      <Header />
       <Routes>
-        <Route path="/" />
-        <Route path="/login" />
-        <Route path="/signup" />
-        <Route path="/post/:postId" />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/post/:postId" element={<PostDetails />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
