7a34e2bd970591367358092ec718b75cad10992b
RGPin <157803400+RGPin@users.noreply.github.com>
2026-07-17 14:09:43 +0800

    signup page

--- client/src/pages/SignupPage/SignupPage.tsx
+++ client/src/pages/SignupPage/SignupPage.tsx
@@ -1,14 +1,72 @@
+import "./SignupPage.css";
+import { useState } from "react";
 import { useSignUp } from "../../hooks/authQueries";
 
 export default function SignupPage() {
+  const [formData, setFormData] = useState({
+    email: "",
+    name: "",
+  });
+
   const signupQuery = useSignUp();
-  const testSignup = () => {
-    signupQuery.mutate({ email: "first@user.com" });
+
+  const handleFormInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   };
+
+  const handleSubmit = (e: React.SubmitEvent) => {
+    e.preventDefault();
+
+    const email = formData.email.trim();
+    if (!email) return;
+
+    signupQuery.mutate({ ...formData });
+    setFormData((prev) => ({
+      ...prev,
+      email: "",
+      name: "",
+    }));
+  };
+
+  // toast for error
+
   return (
     <div className="signup">
-      <h1>Signup</h1>
-      <button onClick={testSignup}>Test</button>
+      <div className="signup-container">
+        <h2 className="signup-title">Create an account</h2>
+        <form className="signup-form" onSubmit={handleSubmit}>
+          <div className="form-field">
+            <label htmlFor="email">Email: </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleFormInput}
+              placeholder="Email"
+            />
+          </div>
+
+          <div className="form-field">
+            <label htmlFor="name">Name: </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleFormInput}
+              placeholder="Name"
+            />
+          </div>
+          <button type="submit">
+            {signupQuery.isPending ? "Loading" : "Sign Up"}
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
