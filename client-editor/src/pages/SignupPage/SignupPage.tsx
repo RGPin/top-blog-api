5106d09bd89987dc7ef607e0a9f98636d7f162d3
RGPin <157803400+RGPin@users.noreply.github.com>
2026-07-20 16:22:41 +0800

    move logout, login and signup pages and functionality to client-editor

--- client-editor/src/pages/SignupPage/SignupPage.tsx
+++ client-editor/src/pages/SignupPage/SignupPage.tsx
@@ -1,3 +1,80 @@
+import "./SignupPage.css";
+import { useState } from "react";
+import { useSignUp } from "../../hooks/authQueries";
+import { Link, useNavigate } from "react-router";
+
 export default function SignupPage() {
-  return <h1>Signup</h1>;
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: "",
+    name: "",
+  });
+
+  const signupQuery = useSignUp();
+
+  const handleFormInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.SubmitEvent) => {
+    e.preventDefault();
+
+    const email = formData.email.trim();
+    if (!email) return;
+
+    signupQuery.mutate({ ...formData });
+    setFormData((prev) => ({
+      ...prev,
+      email: "",
+      name: "",
+    }));
+    navigate("/login");
+  };
+
+  // toast for error
+
+  return (
+    <div className="signup">
+      <div className="signup-container">
+        <h2 className="signup-title">Create an account</h2>
+        <form className="signup-form" onSubmit={handleSubmit}>
+          <div className="form-field">
+            <label htmlFor="email">Email: </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleFormInput}
+              placeholder="Email"
+              disabled={signupQuery.isPending}
+            />
+          </div>
+
+          <div className="form-field">
+            <label htmlFor="name">Name: </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleFormInput}
+              placeholder="Name"
+              disabled={signupQuery.isPending}
+            />
+          </div>
+          <button type="submit" disabled={signupQuery.isPending}>
+            {signupQuery.isPending ? "Loading" : "Sign Up"}
+          </button>
+        </form>
+        <p>
+          Already have an account? <Link to="/login">Sign In</Link>
+        </p>
+      </div>
+    </div>
+  );
 }
---
+++ client-editor/src/types.tsx
@@ -0,0 +1,83 @@
+export type User = {
+  name: string | null;
+  id: number;
+  email: string;
+};
+
+export type SignupResponse = {
+  user: User;
+};
+
+export type Post = {
+  id: number;
+  authorId: number;
+  title: string;
+  content: string;
+  published: boolean;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type PostWithAuthor = {
+  id: number;
+  authorId: number;
+  title: string;
+  content: string;
+  published: boolean;
+  createdAt: string;
+  updatedAt: string;
+  author: Pick<User, "name">;
+};
+
+export type PostsResponse = {
+  posts: PostWithAuthor[];
+};
+
+export type PostDetails = {
+  id: number;
+  title: string;
+  content: string;
+  published: boolean;
+  createdAt: string;
+  updatedAt: string;
+  authorId: number;
+
+  author: {
+    id: number;
+    name: string | null;
+  };
+
+  comments: {
+    id: number;
+    content: string;
+    createdAt: string;
+    updatedAt: string;
+
+    author: {
+      id: number;
+      name: string | null;
+    };
+  }[];
+};
+
+export type PostDetailsResponse = {
+  postDetails: PostDetails;
+};
+
+export type Comment = {
+  id: number;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+
+  author: {
+    id: number;
+    name: string | null;
+  };
+};
+
+export type AccessToken = string;
+
+export type LoginRefreshResponse = {
+  accessToken: AccessToken;
+};
