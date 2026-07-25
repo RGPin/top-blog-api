9dc46e90c796c437b80672af0f165de10cafdfaa
RGPin <157803400+RGPin@users.noreply.github.com>
2026-07-25 11:49:01 +0800

    styling: main page, app padding

--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -1,9 +1,10 @@
+import "./MainPage.css";
 import PostList from "../../components/PostList/PostList";
 
 export default function MainPage() {
   return (
     <div className="main-page">
-      <h1>Main</h1>
+      <h1>Browse The Latest Posts</h1>
       <PostList />
     </div>
   );
